66ddaec90288b0d67cecad2206dd99482cff2fc3
unknown <kavtara123@gmail.com>
2020-10-02 14:49:45 +0400

    https

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.use(express.static('./dist/ccom'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/ccom/'}),
-);
-
-app.listen(process.env.PORT || 8080);
-
-
-
-// var sslRedirect = require('heroku-ssl-redirect');
-// var express = require('express');
-// var app = express();
 // app.use(express.static('./dist/ccom'));
- 
-// app.use(sslRedirect());
- 
-// app.get('/', function(req, res){
 
-//   res.sendFile('index.html', {root: 'dist/ccom/'})
-// });
+// app.get('/*', (req, res) =>
+//   res.sendFile('index.html', {root: 'dist/ccom/'}),
+// );
 
-// app.listen(process.env.PORT || 3000);
+// app.listen(process.env.PORT || 8080);
+import sslRedirect from 'heroku-ssl-redirect';
+import express from 'express';
+
+var sslRedirect = require('heroku-ssl-redirect');
+var express = require('express');
+var app = express();
+app.use(express.static('./dist/ccom'));
+ 
+app.use(sslRedirect());
+ 
+app.get('/', function(req, res){
+
+  res.sendFile('index.html', {root: 'dist/ccom/'})
+});
+
+app.listen(process.env.PORT || 3000);
