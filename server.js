34be2baf8a0150664932d75884e2f3879a3ae955
unknown <kavtara123@gmail.com>
2020-10-02 18:10:26 +0400

    fo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.use(express.static('./dist/ccom'));
+// app.use(express.static('./dist/ccom'));
 
 app.use((req, res, next) => {
   console.log(123);
