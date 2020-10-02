4cac0945d7a5b6899c98ac58fca0436a9e82ab80
unknown <kavtara123@gmail.com>
2020-10-02 12:59:21 +0400

    https-redirect

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.use(express.static('./dist/ccom'));
+// app.use(express.static('./dist/ccom'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/ccom/'}),
-);
+// app.get('/*', (req, res) =>
+//   res.sendFile('index.html', {root: 'dist/ccom/'}),
+// );
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+
+
+
+var sslRedirect = require('heroku-ssl-redirect');
+var express = require('express');
+var app = express();
+ 
+app.use(sslRedirect());
+ 
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
