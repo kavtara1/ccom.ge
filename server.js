e7e442f38f60d5dec9423b4d371c97b69e6fc4be
kavtara1 <kavtara123@gmail.com>
2020-10-14 12:09:58 +0400

    server-config-changes

--- server.js
+++ server.js
@@ -1,58 +1,14 @@
-// import express, { static } from 'express';
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
 
-
-
-// app.use((req, res, next) => {
-//   console.log(123);
-//   let jora = "https://" + req.headers.host + req.url
-//   console.log(jora);
-//   res.redirect(jora)
-// });
-
 app.use(express.static('./dist/ccom'));
 
-app.get('/', (req, res) => {
-  console.log(1111111111111111111111);
+app.get('*', (req, res) => {
   res.sendFile('index.html', { root: 'dist/ccom/' });
-  
-});
 
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
-// import sslRedirect from 'heroku-ssl-redirect/dist/index.js';
-// import express from 'express';
-
-// Object.defineProperty(exports, "__esModule", { value: true });
-// const sslRedirect = (environments = ["production"], status = 302) => {
-//     const currentEnv = process.env.NODE_ENV;
-//     const isCurrentEnv = environments.includes(currentEnv);
-//     return (req, res, next) => {
-//         if (isCurrentEnv) {
-//             req.headers["x-forwarded-proto"] !== "https"
-//                 ? res.redirect(status, "https://" + req.hostname + req.originalUrl)
-//                 : next();
-//         }
-//         else
-//             next();
-//     };
-// };
-
-// var sslRedirect = require('heroku-ssl-redirect');
-// var express = require('express');
-// var app = express();
-// app.use(express.static('./dist/ccom'));
-
-// app.use(sslRedirect());
- 
-// app.get('/', function(req, res){
-//   res.sendFile('index.html', {root: 'dist/ccom/'})
-// });
-
-// app.listen(process.env.PORT || 3000);
